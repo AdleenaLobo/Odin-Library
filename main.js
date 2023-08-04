8ad6311b4670e3a34a792bf0974f5a42524fdd8d
AdleenaLobo <1069455+AdleenaLobo@users.noreply.github.com>
2023-08-04 07:25:13 +0530

    Bugs when tried to add a form

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('body');
 
 let array = [];
 
@@ -6,29 +7,54 @@ function Book()
     let author;
     let no_of_pages;
     let read;
-    this.form = function ()
-    {
-       this.title = prompt("Enter the title");
-        this.author = prompt("Enter the author name");
-        this.no_of_pages = prompt ("Enter no of page");
-        this.read = prompt("ENter status");
-    };
+    let temp;
+    let bun;
+
     this.info = function()
     {
         console.log("Title:"+ this.title+ " Author:"+this.author+" No_of_pages:"+this.no_of_pages+" Status:"+this.read);
     }
 }
+
+Book.prototype.form = function ()
+{
+    const t = document.createElement('div');
+    t.innerHTML = `<form><label for = "title">Enter the title: </label><input type = "text" id = "title">
+    <label for = "author">Enter the author name: </label><input type = "text" id = "author">
+    <label for = "no_of_pages">Enter the number of pages: </label><input type = "text" id = "no_of_pages">
+    <label for = "status">Enter the status: </label><input type = "text" id = "status"></form>`;
+    t.classList.add('fornmEnable');
+    body.appendChild(t);
+
+   this.title = document.getElementById('title').value;
+    this.author = document.getElementById('author').value;
+    this.no_of_pages = document.getElementById('no_of_pages').value;
+    this.read = document.getElementById('status').value;
+};
+
 const article = document.querySelector('article');
 
 Book.prototype.callArray= function()
 {
-    let temp = document.createElement('div');
-    temp.classList.add('pluck-card');
-    
-        this.tempContent(temp);
-        article.appendChild(temp);
+    this. temp = document.createElement('div');
+    this.temp.classList.add('pluck-card');
+
+    let outerbun = document.createElement('div');
+    this. bun = document.createElement('button');
+
+    this.bun.textContent = 'delete';
+    this.bun.classList.add('spec-bun');
+        this.tempContent(this.temp);
+        outerbun.appendChild(this.bun);
+        this.temp.appendChild(outerbun);
+        article.appendChild(this.temp);
+
+        this.bun.addEventListener('click', ()=>{
+            article.removeChild(this.temp);
+        })
 
 }
+
 
 Book.prototype.tempContent= function(temp)
 {
@@ -69,4 +95,5 @@ button.addEventListener('click', ()=>
     array.push(obj);
     obj.callArray();
 })
+
 
