111a7d2bba7a9cbb6d76b8750372797e30754ba6
AdleenaLobo <1069455+AdleenaLobo@users.noreply.github.com>
2023-09-17 17:59:03 +0530

    Refactor  javascript code with classes

--- main.js
+++ main.js
@@ -1,141 +1,202 @@
-const body = document.querySelector('body');
 
-const article = document.querySelector('article');
-
-let array = [];
-
-let temp = document.createElement('div');
-
-
-function Book()
-{   let title;
-    let author;
-    let no_of_pages;
-    let read;
-    let temp;
-    let bun;
-    let formm;
-    let outer_form;
-
-}
-
-const button = document.querySelector('.Add');
-button.addEventListener('click', ()=>
+class BaseOf
 {
-    let obj = new Book();
-    obj.form();
-});
-
-Book.prototype.form = function ()
-{
-    this.outer_form = document.createElement('div');
-    this.formm = document.createElement('div');
-
-    this.formm.innerHTML = `<form><label for = "title">Enter the title: </label><input type = "text" id = "title">
-    <label for = "author">Enter the author name: </label><input type = "text" id = "author">
-    <label for = "no_of_pages">Enter the number of pages: </label><input type = "text" id = "no_of_pages">
-    <label for = "status">Enter the status: </label><input type = "text" id = "status">
-    <button type = "button" id = "bun">Submit</button></form>`;
-
-    this.outer_form.appendChild(this.formm);
-
-    body.classList.add('body1');
-    this.outer_form.classList.add('formEnable');
-
-    body.appendChild(this.outer_form);
-
-    const button1 = document.getElementById('bun');
-    button1.addEventListener('click', ()=>
+    constructor()
     {
         
-    this.info();
-    body.classList.remove('body1');
-   body.removeChild(this.outer_form);
- 
-    article.removeChild(temp);
-    article.classList.remove('temop');
+    this.body = document.querySelector('body');
+    this.article = document.querySelector('article');
+    this.array=[];
+    this.temp = document.createElement('div');
+    }
+    getbod()
+    {
+        return this.body;
+    }
 
-    });
-};
+    getarticle()
+    {
+        return this.article;
+    }
 
-Book.prototype.info = function()
-{
-    
-    this.title = document.getElementById('title').value;
-    this.author = document.getElementById('author').value;
-    this.no_of_pages = document.getElementById('no_of_pages').value;
-    this.read = document.getElementById('status').value;
+    getarray()
+    {
+        return this.array;
+    }
 
-    
-    array.push([this.title , this.author, this.no_of_pages , this.read]);
-    console.log("Title:"+ this.title+ " Author:"+this.author+" No_of_pages:"+this.no_of_pages+" Status:"+this.read);
-    
-    this.callArray();
+    gettemp()
+    {
+        return this.temp;
+    }
 
+    check()
+    {
+        if(this.array.length == 0 )
+        {
+            this.article.classList.add('temop');
+            this.temp.textContent= `The shelf is empty`;
+            this.article.appendChild(this.temp);
+        }
+
+    }
+
+    remove_empty_shelf()
+    {
+        this.temp = document.querySelector('.temop>div');
+        if(this.temp != undefined)
+        {
+        console.log(this.temp);
+        this.article.removeChild(this.temp);
+        this.article.classList.remove('temop');
+        }
+
+    }
 }
 
 
-Book.prototype.callArray= function()
+class book extends BaseOf
 {
+    constructor(title , author, no_of_pages , read)
+    {
+        
+        super();
+        this.title = title;
+        this.author = author;
+        this.no_of_pages = no_of_pages;
+        this.read = read; 
+        this.pusharry();
+        this.add();
+        this.bun.addEventListener('click', this.remove.bind(this));
+    }
+
+    add()
+    {
     this.temp = document.createElement('div');
     this.temp.classList.add('pluck-card');
 
     this.outerbun = document.createElement('div');
     this.bun = document.createElement('button');
-
+    this.tempContent();
     this.bun.textContent = 'delete';
     this.bun.classList.add('spec-bun');
 
-        this.tempContent(this.temp);
+        
         this.outerbun.appendChild(this.bun);
         this.temp.appendChild(this.outerbun);
-        article.appendChild(this.temp);
+        this.article.appendChild(this.temp);
 
-        this.bun.addEventListener('click', ()=>{
-            console.log(
-                array.pop());
-            article.removeChild(this.temp);
-            check();
-        });
-
-};
-
-
-Book.prototype.tempContent= function(temp)
-{
-    for (let i =0 ; i<4 ; i++)
-    {
-        
-    let divi = document.createElement('p');
-        switch(i)
-        {
-            case 0: 
-            divi.textContent = `Title: ${this.title}`;
-            break;
-
-            case 1:
-            divi.textContent = `Author: ${this.author}`;
-            break;
-
-            case 2:
-            divi.textContent = `Pages: ${this.no_of_pages} pages`;
-            break;
-
-            case 3:
-            divi.textContent = `Status: ${this.read}`;
-            break;
-        }
-        temp.appendChild(divi);
     }
 
+    remove()
+    {
+        this.temp= document.querySelector('.pluck-card');
+        this.temp.classList.remove('pluck-card');
+       this.article.removeChild(this.temp);
+        this.array= this.getarray();
+        this.array.pop();
+        this.check();
+    }
+
+    pusharry()
+    {
+        
+    this.array.push([this.title , this.author, this.no_of_pages , this.read]);
+    }
+
+    tempContent()
+    {
+        this.temp.innerHTML = `Title: ${this.title}<br><br>
+        Author: ${this.author} <br><br>
+        No of pages: ${this.no_of_pages}<br><br>
+        Status: ${this.read}<br><br>`;
+    }
 
 }
-function check(){
-if(array.length == 0 )
+
+class form extends BaseOf
 {
-    article.classList.add('temop');
-    temp.textContent= `The shelf is empty`;
-    article.appendChild(temp);
-}}
-check();
-console.log(array);
+    constructor()
+    {
+        super();
+        this.remove_empty_shelf();
+        this.createMod();
+        this.addText();
+        this.appendMod();
+        this.body = this.getbod();
+        console.log(this.body);
+        document.getElementById('bun').addEventListener('click' ,this.makeobjbook.bind(this));
+        /*this.body.classList.remove('body1');
+        this.body.removeChild(this.outer_form);
+        this.article.removeChild(this.temp);
+        this.article.classList.remove('temop');
+    */}
+
+    createMod()
+    {        
+    this.outer_form = document.createElement('div');
+    this.formelem = document.createElement('div');
+    }
+
+    addText()
+    {
+
+        this.formelem.innerHTML = `<form><label for = "title">Enter the title: </label><input type = "text" id = "title">
+        <label for = "author">Enter the author name: </label><input type = "text" id = "author">
+        <label for = "no_of_pages">Enter the number of pages: </label><input type = "text" id = "no_of_pages">
+        <label for = "status">Enter the status: </label><input type = "text" id = "status">
+        <button type = "button" id = "bun">Submit</button></form>`;
+    }
+
+    appendMod()
+    {
+        
+        this.outer_form.appendChild(this.formelem);
+    
+        this.body.classList.add('body1');
+        this.outer_form.classList.add('formEnable');
+    
+        this.body.appendChild(this.outer_form);
+
+    }
+    
+    removeElem()
+    {
+        this.outer_form.removeChild(this.formelem);
+        this.outer_form.classList.remove('formEnable');
+        this.body.classList.remove('body1');
+        this.body.removeChild(this.outer_form);
+    }
+
+    makeobjbook()
+    {
+
+         this.book1 = new book(document.getElementById('title').value ,document.getElementById('author').value
+        ,document.getElementById('no_of_pages').value, document.getElementById('status').value );
+        
+        this.outer_form = document.querySelector('.formEnable');
+        this.body.classList.remove('body1');
+        this.body.removeChild(this.outer_form);
+    }
+
+}
+
+let StartPage =
+( 
+     init = (function()
+     {
+    this.button = document.querySelector('.Add')
+    })(),
+
+    form1 = function()
+    {
+        formobj = new form();
+    },
+    
+    callBO =(function()
+    {
+        bobj= new BaseOf().check();
+    })(),
+
+    this.button.addEventListener('click' , form1)
+    
+);
